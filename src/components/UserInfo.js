1b10cac529fb9ad60a8a24bffc87000ee1c257e0
AJRusch <155108459+AJRusch@users.noreply.github.com>
2024-05-30 13:33:45 -0500

    Add files via upload

---
+++ src/components/UserInfo.js
@@ -0,0 +1,16 @@
+export default class UserInfo {
+  constructor(titleSelector, descriptionSelector) {
+    this._profileTitle = document.querySelector(titleSelector);
+    this._profileDescription = document.querySelector(descriptionSelector);
+  }
+  getUserInfo() {
+    return {
+      title: this._profileTitle.textContent,
+      description: this._profileDescription.textContent,
+    };
+  }
+  setUserInfo(data) {
+    this._profileTitle.textContent = data.title;
+    this._profileDescription.textContent = data.description;
+  }
+}
