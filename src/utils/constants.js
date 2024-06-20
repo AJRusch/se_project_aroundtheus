e0893bc0e6e6a5a9e5319177fe02265739531f9f
AJRusch <155108459+AJRusch@users.noreply.github.com>
2024-06-20 14:48:19 -0500

    Add files via upload
    Like click revision

---
+++ src/utils/constants.js
@@ -0,0 +1,50 @@
+export const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg ",
+  },
+];
+
+export const validationSettings = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__button",
+  inactiveButtonClass: "modal__button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error_active",
+  modalSpan: ".modal__span",
+};
+
+export const profileEditButton = document.querySelector("#profile-edit-button");
+export const profileTitleInput = document.querySelector("#person-name");
+export const profileDescriptionInput = document.querySelector(
+  "#person-description"
+);
+export const profileEditForm = document.querySelector("#profile-edit-form");
+export const cardSection = document.querySelector(".cards");
+export const cardAddButton = document.querySelector("#add-button");
+export const cardAddForm = document.querySelector("#card-add-form");
+export const editAvatarButton = document.querySelector(
+  ".profile__image-overlay"
+);
+export const editAvatarForm = document.querySelector("#avatar-edit-form");
